4a5e60a490e6aabfef6072130debbe6a7212b383
tt-11 <abhishek@wearetechtonic.com>
2024-03-11 18:37:51 +0530

    commit by abhishek sharma after adding spring security using jwt

---
+++ src/main/java/com/techtonic/vp/controllers/AuthenticationController.java
@@ -0,0 +1,68 @@
+package com.techtonic.vp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techtonic.vp.payloads.JwtRequestDto;
+import com.techtonic.vp.payloads.JwtResponseDto;
+import com.techtonic.vp.security.JwtHelper;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private JwtHelper helper;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponseDto> login(@RequestBody JwtRequestDto request) {
+
+        this.doAuthenticate(request.getEmail(), request.getPassword());
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+        String token = this.helper.generateToken(userDetails);
+
+        JwtResponseDto response = JwtResponseDto.builder()
+                .jwtToken(token)
+                .username(userDetails.getUsername()).build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    private void doAuthenticate(String email, String password) {
+
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(email, password);
+        try {
+            manager.authenticate(authentication);
+
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException(" Invalid Username or Password  !!");
+        }
+
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public String exceptionHandler() {
+        return "Credentials Invalid !!";
+    }
+
+}
---
+++ src/main/java/com/techtonic/vp/payloads/JwtRequestDto.java
@@ -0,0 +1,14 @@
+package com.techtonic.vp.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtRequestDto {
+
+    private String email;
+    private String password;
+}
